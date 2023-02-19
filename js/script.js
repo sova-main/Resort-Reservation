9a7d2477f7ba3e29d67655f93c90afbcecf77327
Sova <boquiren40@gmail.com>
2023-02-20 03:00:35 +0800

    Fixing Overflow (1)

--- js/script.js
+++ js/script.js
@@ -32,3 +32,21 @@ const currentYear = new Date().getFullYear();
 
 year.textContent = currentYear;
 
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3, 
+    },
+  },
+});
