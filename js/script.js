6cdedefbaf1c82519b8c16428e71b109b2749839
Sova <boquiren40@gmail.com>
2023-02-20 00:25:58 +0800

    Home page done

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{ 
+    navbar.classList.remove('active');
+}
+
+// Disable past date
+const checkInInput = document.getElementById('date-checkin');
+const checkOutInput = document.getElementById('date-checkout');
+
+var today = new Date().toISOString().split('T')[0];
+
+checkInInput.setAttribute('min', today);
+checkOutInput.setAttribute('min', today);
+
+
+checkInInput.addEventListener('input', (event) => {
+  const selectedDate = new Date(event.target.value);
+  const minDate = new Date(selectedDate.getTime() + 24 * 60 * 60 * 1000);
+  checkOutInput.min = minDate.toISOString().split('T')[0];
+});
+
+
+// get copyright year
+
+const year = document.getElementById('year');
+const currentYear = new Date().getFullYear();
+
+year.textContent = currentYear;
+
